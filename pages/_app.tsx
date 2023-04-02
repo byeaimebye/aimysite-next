6d5cd5fc8c4592a5b26896a1826eedc162c55bf0
Aime Pelozo <aime.pelozo@celeri.app>
2023-04-02 20:48:37 -0300

    build docker image and add next ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import MainLayout from "@/components/layouts/MainLayout"
 import "@/styles/globals.css"
+import { NextUIProvider } from "@nextui-org/react"
 import type { AppProps } from "next/app"
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <MainLayout>
-            <Component {...pageProps} />
-        </MainLayout>
+        <NextUIProvider>
+            <MainLayout>
+                <Component {...pageProps} />
+            </MainLayout>
+        </NextUIProvider>
     )
 }
